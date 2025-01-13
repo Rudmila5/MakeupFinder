47ebdb51fadc5541b9490db074a53fc7e6885733
Rudmila <157866574+Rudmila5@users.noreply.github.com>
2025-01-13 10:51:38 -0500

    Update results.js

--- results.js
+++ results.js
@@ -30,22 +30,31 @@ function displayResults(data) {
   });
 }
 
+const apiBaseURL = 'https://makeupfinder.onrender.com/';
 
-function fetchSearchResults(query) {
-  document.getElementById('loading').style.display = 'block'; 
-  
-  console.log('Fetching data for query:', query);
-  
-  fetch(`http://localhost:4000/search?query=${encodeURIComponent(query)}`)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('loading').style.display = 'none'; 
-      console.log('Data received:', data);
-      displayResults(data); 
-    })
-    .catch(error => {
-      console.error('Error fetching products:', error);
-      document.getElementById('loading').style.display = 'none';
-      document.getElementById('results-container').innerHTML = '<p>Error fetching products.</p>';
-    });
+async function fetchSearchResults(query) {
+  console.log(`Fetching data for query: ${query}`);
+  try {
+    const response = await fetch(`${apiBaseURL}/search?query=${query}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('Search results:', data);
+    displayResults(data); // Function to render the results on your frontend
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    displayErrorMessage('Error fetching products. Please try again.');
+  }
 }
+
+function displayErrorMessage(message) {
+  const resultsContainer = document.getElementById('results-container');
+  resultsContainer.innerHTML = `<p>${message}</p>`;
+}
+
+
+
+
+
+
