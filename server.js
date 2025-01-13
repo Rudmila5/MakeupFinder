76601328aa0e2a823244e4cc53fbfe99bef416e0
Rudmila <157866574+Rudmila5@users.noreply.github.com>
2025-01-13 10:42:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(cors({
 
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Rudu5@', 
-  database: 'ingredientfinder', 
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, 
 });
 
 
