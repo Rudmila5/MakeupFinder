150e93d451173e40000d1b05e505656e75393b6c
Rudmila5 <rudmila138@gmail.com>
2025-01-16 20:58:53 -0500

    localhost

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
+console.log(process.env.DB_HOST); 
+console.log(process.env.DB_USER); 
+
 
 const corsOptions = {
   origin: [
