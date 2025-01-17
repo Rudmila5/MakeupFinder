c3011bd6cbca6a870bcf26fddce59a12b633061b
Rudmila5 <rudmila138@gmail.com>
2025-01-16 21:12:46 -0500

    local host

--- results.js
+++ results.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const apiBaseURL = 'http://localhost:4000';
 
   const urlParams = new URLSearchParams(window.location.search);
-  const searchQuery = urlParams.get('query');
+  const searchQuery = urlParams.get('search');
   console.log('URL Search Query:', searchQuery);
 
   if (searchQuery) {
