8bb192e4360b5ca4faeceea71fd0c1083faf5363
Rudmila5 <rudmila138@gmail.com>
2025-01-19 16:11:10 -0500

    hosting

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOST || 'monorail.proxy.rlwy.net',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Rudu5@',
-  database: process.env.MYSQL_DATABASE || 'ingredientfinder',
+  password: process.env.MYSQL_PASSWORD || 'your_password_here',
+  database: process.env.MYSQL_DATABASE || 'railway',
   connectionLimit: 10,
 });
 
